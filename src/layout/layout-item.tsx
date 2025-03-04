8c12c8ec259a4bdad47b094f4e1a8776a3972a1f
MUHAMMADAZIZ <muhammadazizgulomov9@gmail.com>
2025-03-04 18:12:59 +0500

    first commit

---
+++ src/layout/layout-item.tsx
@@ -0,0 +1,9 @@
+import { HomeOutlined } from "@ant-design/icons";
+
+export const items = [
+  {
+    key: "1",
+    icon: <HomeOutlined />,
+    label: "Home",
+  },
+];
---
+++ src/layout/main-layout.tsx
@@ -0,0 +1,57 @@
+import { Button, Layout, Menu, theme } from "antd";
+import Sider from "antd/es/layout/Sider";
+import { items } from "./layout-item";
+import { Content, Header } from "antd/es/layout/layout";
+import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+
+
+
+const MainLayout = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <Layout style={{minHeight: '100vh'}}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: "16px",
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default MainLayout;
